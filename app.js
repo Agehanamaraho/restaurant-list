71aabcb2805dd5e07e4f191bbfb3ebcd36167833
Agehanamaraho <r2anniekao@gmail.com>
2022-01-22 22:27:24 +0800

    Initialize Project

---
+++ app.js
@@ -0,0 +1,37 @@
+//package used in this project
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+
+//setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+//setting static files
+app.use(express.static('public'))
+
+//setting routers
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => {
+    return restaurant.id.toString() === req.params.restaurant_id
+  })
+  res.render('show', { restaurant: restaurant })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword.trim().toLowerCase()
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword) || restaurant.category.includes(keyword)
+  })
+  res.render('index', { restaurants: restaurants })
+})
+
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
