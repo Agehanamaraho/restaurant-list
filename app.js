f5892b129e49a7356d22498503a02e799c9f9c47
Agehanamaraho <r2anniekao@gmail.com>
2022-01-30 18:58:28 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
-//package used in this project
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
 
-//setting mongoose
-mongoose.connect('mongodb://localhost/restaurant_list')
-
-const db = mongoose.connection
-
-db.on('error', () => console.log('mongodb error'))
-db.once('open', () => console.log('mongodb connected'))
-
-//setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
--- models/seeds/restaurantListSeeder.js
+++ models/seeds/restaurantListSeeder.js
@@ -1,12 +1,7 @@
-const mongoose = require('mongoose')
 const Restaurant = require('../restaurant')
 const restaurantList = require('../../restaurant.json')
+const db = require('../../config/mongoose')
 
-mongoose.connect('mongodb://localhost/restaurant_list')
-
-const db = mongoose.connection
-
-db.on('error', () => console.log('mongodb error'))
 db.once('open', () => {
   console.log('mongodb connected')
   Restaurant.create(restaurantList.results)
